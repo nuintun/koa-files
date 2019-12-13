fb75d4efceca71a8527a9c309a0a44a2cd17ed56
nuintun <nuintun@qq.com>
2019-12-13 11:06:50 +0800

    :art: Update files

--- tests/server.js
+++ tests/server.js
@@ -10,8 +10,20 @@ const server = require('../index');
 const app = new Koa();
 const port = process.env.PORT || 80;
 
+/**
+ * @function socketError
+ * @param {NodeJS.ErrnoException} error
+ * @returns {boolean }
+ */
+function socketError(error) {
+  return /ECONNABORTED|ECONNRESET|EPIPE|ETIMEDOUT|ENOPROTOOPT/.test(error.code);
+}
+
+// Static files server
 app.use(server('tests'));
 
-app.on('error', error => !/ECONNRESET|ECANCELED|ECONNABORTED/.test(error.code) && console.error(error));
+// Listen error event
+app.on('error', error => !socketError(error) && console.error(error));
 
+// Start server
 app.listen(port, () => console.log(`> server running at: 127.0.0.1:${port}`));
