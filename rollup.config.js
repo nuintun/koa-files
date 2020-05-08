4f2642632da1ac9f5b5546c75272f0f5c70368f3
nuintun <nuintun@qq.com>
2020-05-08 18:44:38 +0800

    :construction_worker: Update tools

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+/**
+ * @module rollup.config
+ */
+
+import rimraf from 'rimraf';
+import pkg from './package.json';
+import typescript from 'rollup-plugin-typescript2';
+
+rimraf.sync('typings');
+rimraf.sync('index.js');
+
+const banner = `/**
+ * @module ${pkg.name}
+ * @license ${pkg.license}
+ * @version ${pkg.version}
+ * @author ${pkg.author.name}
+ * @description ${pkg.description}
+ * @see ${pkg.homepage}
+ */
+`;
+
+const tsconfigOverride = { compilerOptions: { declaration: true, declarationDir: 'typings' } };
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    banner,
+    indent: true,
+    strict: true,
+    format: 'cjs',
+    interop: false,
+    esModule: false,
+    file: 'index.js',
+    preferConst: true
+  },
+  external: ['tslib', 'ms', 'etag', 'destroy', 'range-parser', 'fs', 'path', 'stream'],
+  plugins: [typescript({ tsconfigOverride, clean: true, useTsconfigDeclarationDir: true })]
+};
