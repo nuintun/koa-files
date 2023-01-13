4401027bd80e45b436ee5a64ae7d73a8e7b059bd
nuintun <nuintun@qq.com>
2023-01-13 10:54:52 +0800

    chore: Update tools

--- tools/clean.js
+++ tools/clean.js
@@ -4,14 +4,4 @@
 
 import rimraf from 'rimraf';
 
-function clean(paths) {
-  if (Array.isArray(paths)) {
-    for (const path of paths) {
-      rimraf.sync(path);
-    }
-  } else {
-    rimraf.sync(path);
-  }
-}
-
-clean(['cjs', 'esm', 'types']);
+rimraf.sync(['cjs', 'esm', 'types']);
