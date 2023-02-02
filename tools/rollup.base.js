5c43680c4ad681e5094982f50a63a79293d2aa45
nuintun <nuintun@qq.com>
2023-02-02 14:38:53 +0800

    chore: Update tools

--- tools/rollup.base.js
+++ tools/rollup.base.js
@@ -21,7 +21,7 @@ const banner = `/**
 /**
  * @function rollup
  * @param esnext
- * @param development
+ * @return {import('rollup').RollupOptions}
  */
 export default function rollup(esnext) {
   return {
