0b97fed9c709a0304da5c6b9d22d34897de4be54
nuintun <nuintun@qq.com>
2026-04-08 11:22:26 +0800

    refactor(fs): update FileSystem interface with better type definitions
    - Add PathLike type import and use it for stat and open methods
    - Define Position type as number | bigint | null for read method
    - Define OpenMode type as string | number | undefined for open method
    - Make Callback error parameter optional using ? operator
    - Change Buffer<ArrayBuffer> to ArrayBufferView for better compatibility
    - Make callback parameters optional in close and read methods

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -2,25 +2,28 @@
  * @module fs
  */
 
-import { Stats } from 'node:fs';
-import { Buffer } from 'node:buffer';
+import { PathLike, Stats } from 'node:fs';
+
+type Position = number | bigint | null;
+
+type OpenMode = string | number | undefined;
 
 interface Callback<T extends unknown[] = []> {
-  (error: Error | null, ...rest: T): void;
+  (error?: Error | null, ...rest: T): void;
 }
 
 export interface FileSystem {
-  close(fd: number, callback: Callback): void;
-  read<T extends Buffer<ArrayBuffer>>(
+  close(fd: number, callback?: Callback): void;
+  read<T extends ArrayBufferView>(
     fd: number,
     buffer: T,
     offset: number,
     length: number,
-    position: number,
+    position: Position,
     callback: Callback<[bytesRead: number, buffer: T]>
   ): void;
-  stat(path: string, callback: Callback<[stats: Stats]>): void;
-  open(path: string, flags: string, callback: Callback<[fd: number]>): void;
+  stat(path: PathLike, callback: Callback<[stats: Stats]>): void;
+  open(path: PathLike, flags: OpenMode, callback: Callback<[fd: number]>): void;
 }
 
 /**
